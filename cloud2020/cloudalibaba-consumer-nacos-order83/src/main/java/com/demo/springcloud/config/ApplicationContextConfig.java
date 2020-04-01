fc47f5c773a74f27193d98f2a04dd5caf715ea24
yuguangqi <13802885384@139.com>
2020-04-01 17:09:12 +0800

    nacos服务注册消费者

--- cloud2020/cloudalibaba-consumer-nacos-order83/src/main/java/com/demo/springcloud/config/ApplicationContextConfig.java
+++ cloud2020/cloudalibaba-consumer-nacos-order83/src/main/java/com/demo/springcloud/config/ApplicationContextConfig.java
@@ -1,4 +1,18 @@
 package com.demo.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+//Nacos自带Ribbon，引入RestTemplate
+@Configuration
 public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced //RestTemplate结合Ribbon做负载均衡一定要加@LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 }
