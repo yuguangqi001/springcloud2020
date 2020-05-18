e3e845adb74e4085fabe3690eaf396d1da974e85
yuguangqi <13802885384@139.com>
2020-05-18 16:59:45 +0800

    sentinel--流控规则、降级规则、热点参数规则

--- cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/demo/springcloud/controller/FlowLimitController.java
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/demo/springcloud/controller/FlowLimitController.java
@@ -1,8 +1,13 @@
 package com.demo.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -18,5 +23,29 @@ public class FlowLimitController {
         log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "--------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD() {
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int i = 1 / 0;
+        return "----- testD";
+    }
+
+    //热点限流
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "*****testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "****deal_testHotKey";
+    }
+
 
 }
